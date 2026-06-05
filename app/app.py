36befafdf74df18fed9b336c1b11cf9379f83b98
chirag-sirpi <chirag-sirpi@users.noreply.github.com>
2026-06-05 14:28:53 +0530

    fix: update welcome message text in index endpoint

--- app/app.py
+++ app/app.py
@@ -44,7 +44,7 @@ def log_request(response):
 def index():
     return jsonify({
         "status": "healthy",
-        "message": "Welcome to Chirag GKE DevSecOps microservice! and welcome to Feel it buy",
+        "message": "Welcome to Chirag GKE DevSecOps microservice! and welcome to Feel it",
         "time": time.time()
     })
 
